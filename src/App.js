be05f720ae87f62534defdfd2f7393264293d2c9
aniljayswal8685 <aniljayswal8685@gmail.com>
2022-06-06 22:27:27 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React ,{useState}from "react";
 import "./style.css";
 
 export default function App() {
+
+let [name, setName] = useState('monday')
+let [state, setState] = useState(true)
+function changeName(){
+  setName("tuesday")
+  setState(false)
+}
+
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={changeName}> Change Name</button>
+      <button onClick={()=>{setName('monday')}}> Reset</button>
+      <div>{name}</div>
+      {
+        state
+      ?<div style={{backgroundColor:"red"}}> When there is monday </div>
+      :<div style={{backgroundColor:"green"}}>When there is Tuesday</div>
+
+      }
     </div>
   );
 }
